5bf9bd01ff30335690bcbc8d428fc70d1e9b609b
Jake <jakefantin@gmail.com>
2019-11-11 06:05:16 +0000

    added SQL support to get all reservations of a the logged in user

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -73,7 +73,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(rating) as average_rating
+  FROM reservations 
+  JOIN properties ON property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date DESC
+  LIMIT $2;
+  `,[guest_id, limit])
+  .then(res => {
+    if (res.rows.length < 1) {
+      return null;
+    } else {
+      return res.rows;
+    }
+  });
 }
 exports.getAllReservations = getAllReservations;
 
